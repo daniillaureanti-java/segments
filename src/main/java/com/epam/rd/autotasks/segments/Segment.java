9d7e93d2d30340352399ef0ddcc6f6d3e7fcee48
Daniil Laureanti <daniil.laureanti@gmail.com>
2021-12-02 17:59:24 +0200

    Completed task

--- src/main/java/com/epam/rd/autotasks/segments/Segment.java
+++ src/main/java/com/epam/rd/autotasks/segments/Segment.java
@@ -4,21 +4,52 @@ import static java.lang.Math.abs;
 import static java.lang.Math.sqrt;
 import static java.lang.StrictMath.pow;
 
-class Segment {
+public class Segment {
+    Point start, end;
 
     public Segment(Point start, Point end) {
-
+        this.start = start;
+        this.end = end;
+        if (start.getX() == end.getX() && start.getY() == end.getY()) {
+            throw new IllegalArgumentException("Start and end points are equal");
+        }
     }
 
     double length() {
-
+        double distanceX = StrictMath.pow(start.getX() - end.getX(), 2);
+        double distanceY = StrictMath.pow(start.getY() - end.getY(), 2);
+        return Math.sqrt(distanceX + distanceY);
     }
 
-    Point middle() {
-
+    public Point middle() {
+        double x = (start.getX() + end.getX()) / 2;
+        double y = (start.getY() + end.getY()) / 2;
+        return new Point(x, y);
     }
 
-    Point intersection(Segment another) {
+    public Point intersection(Segment another) {
+        double x1 = start.getX();
+        double x2 = end.getX();
+        double x3 = another.start.getX();
+        double x4 = another.end.getX();
+        double y1 = start.getY();
+        double y2 = end.getY();
+        double y3 = another.start.getY();
+        double y4 = another.end.getY();
+
+        double d = ((x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4));
+        if (d != 0) {
+            double t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / d;
+            double u = ((x1 - x3) * (y1 - y2) - (y1 - y3) * (x1 - x2)) / d;
+            if ((0.0 <= t && t <= 1.0) && (0 <= u && u <= 1)) {
+                double x = x1 + t * (x2 - x1);
+                double y = y1 + t * (y2 - y1);
+                return new Point(x, y);
+            } else {
+                return null;
+            }
+        } else return null;
+
 
     }
 
